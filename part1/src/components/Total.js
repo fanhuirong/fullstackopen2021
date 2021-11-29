3ac9e2431d72c09f20bad725459c5a6b062bb3a3
fanhuirong <fanhuirong321@gmail.com>
2021-11-29 16:50:05 +0800

    feat:step 2.3

--- part1/src/components/Total.js
+++ part1/src/components/Total.js
@@ -3,7 +3,8 @@ import React from 'react'
 const Total = ({ parts }) => {
 
   return (
-    <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+    // <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+    <p>Number of exercises {parts.reduce((pre, cur) => pre + cur)}</p>
   )
 }
 
