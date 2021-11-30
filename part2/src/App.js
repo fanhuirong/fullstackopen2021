58f18836554e7c788f70653aa6ffa4cd2e99fca9
fanhuirong <fanhuirong321@gmail.com>
2021-11-30 21:14:42 +0800

    feat: init part2 &step 2.6

---
+++ part2/src/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (e) => {
+    e.preventDefault()
+    const newPerson = {
+      name: newName,
+    }
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+
+  const changeName = (e) => {
+    setNewName(e.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={changeName} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <>
+        {persons.map(item=><p key={item.name}>{item.name}</p>)}
+      </>
+    </div>
+  )
+}
+
+export default App
