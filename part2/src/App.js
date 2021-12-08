1584446f0e82237e60f11fb31a4f98eda9a13462
fanhuirong <fanhuirong321@gmail.com>
2021-12-08 21:48:20 +0800

    feat:step 2.15,2.16

--- part2/src/App.js
+++ part2/src/App.js
@@ -2,26 +2,30 @@ import React, { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
-import axios from 'axios'
+import request from './request'
 
 const App = () => {
   const [persons, setPersons] = useState([])
-  const [filterPersons, setFilterPersons] = useState([])
+  // const [filterPersons, setFilterPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
 
   useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        console.log('promise fulfilled')
-        setPersons(response.data)
-        setFilterPersons(response.data)
-      })
+    // console.log('effect')
+    // axios
+    //   .get('http://localhost:3001/persons')
+    //   .then(response => {
+    //     // console.log('promise fulfilled')
+    //     setPersons(response.data)
+    //     // setFilterPersons(response.data)
+    //   })
+    request.getAll().then(personList => {
+      console.log(personList)
+      setPersons(personList)
+    })
   }, [])
-  console.log('render', persons.length, 'notes')
+  // console.log('render', persons.length, 'notes')
 
   const addPerson = (e) => {
     e.preventDefault()
@@ -33,11 +37,20 @@ const App = () => {
       name: newName,
       number: newNumber
     }
-    setPersons(persons.concat(newPerson))
-    setFilterPersons(persons.concat(newPerson))
-    setNewName('')
-    setNewNumber('')
-    setNewFilter('')
+
+    request.create(newPerson).then(data=>{
+      setPersons(persons.concat(data))
+    })
+
+      // axios
+      //   .post('http://localhost:3001/persons', newPerson)
+      //   .then(response => {
+      //     setPersons(persons.concat(response.data))
+      //     // setFilterPersons(persons.concat(response.data))
+      //     setNewName('')
+      //     setNewNumber('')
+      //     setNewFilter('')
+      //   })
   }
 
   const changeName = (e) => {
@@ -46,23 +59,23 @@ const App = () => {
   const changeNumber = (e) => {
     setNewNumber(e.target.value)
   }
-  const changeFilter = (e) =>{
-    setNewFilter(e.target.value)
-    // const filterArr = persons.filter(item => item.name.indexOf(e.target.value) !== -1)
-    const filterArr = persons.filter(item => item.name.includes(e.target.value))
-    setFilterPersons(filterArr) 
-  }
+  // const changeFilter = (e) =>{
+  //   setNewFilter(e.target.value)
+  //   // const filterArr = persons.filter(item => item.name.indexOf(e.target.value) !== -1)
+  //   const filterArr = persons.filter(item => item.name.includes(e.target.value))
+  //   setFilterPersons(filterArr) 
+  // }
 
   return (
     <div>
       <h2>Phonebook</h2>
-        <div>
+        {/* <div>
           <Filter newFilter={newFilter} changeFilter={changeFilter}/>
-        </div>  
+        </div>   */}
         <h3>Add a new</h3>
         <PersonForm newName={newName} changeName={changeName} newNumber={newNumber}  changeNumber={changeNumber} addPerson={addPerson}/>
       <h2>Numbers</h2>
-      <Persons filterPersons={filterPersons}/>
+      <Persons filterPersons={persons}/>
     </div>
   )
 }
