531d3cfb489672019f0b80727eb02bc28fd95652
fanhuirong <fanhuirong321@gmail.com>
2022-01-06 00:15:36 +0800

    feat: add populate(like join querys)

--- part4/controllers/blog.js
+++ part4/controllers/blog.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 // 引入数据库Schema的model
 const Blog = require('../models/blogs')
+const User = require('../models/users')
 
 // getAll
 // blogsRouter.get('/', (request, response) => {
@@ -29,10 +30,20 @@ blogsRouter.get('/', async (request, response) => {
 // step 4.10
 // async await
 blogsRouter.post('/', async (request, response, next) => {
-  const blog = new Blog(request.body)
-
+  // const blog = new Blog(request.body)
+  const body = request.body
+  const user = await User.findById(body.userId)
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user._id
+  })
   try {
     const savedBlog = await blog.save();
+    user.blogs = user.blogs.concat(savedBlog);
+    await user.save();
     response.status(200).json(savedBlog)
   } catch (error) {
     next(error)
