db33d5e9537d720a10c185ced2b1e9223afb96c6
fanhuirong <fanhuirong321@gmail.com>
2022-01-13 21:18:19 +0800

    feat: step 5.14

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import Togglable from './Togglable'
 const Blog = ({blog}) => (
   <div className="blog">
     <span>{blog.title}</span> {blog.author}
+    <Togglable buttonLabel="show Detail">
+      <div className='hide'>
+      <p>{blog.likes}</p>
+      <p>{blog.url}</p>
+      </div>
+    </Togglable>
   </div>  
 )
 
--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
-test('renders content', () => {
-  const blog = {
-    title: 'Component testing is done with react-testing-library',
-    author: 'kun kun',
-    url: "http://google.com",
-    likes: 9999,
-    user: {
-      username: "Kun kun",
-      name: "The Kun",
-    },
-  }
+const blog = {
+  title: 'Component testing is done with react-testing-library',
+  author: 'kun kun',
+  url: "http://google.com",
+  likes: 9999,
+  user: {
+    username: "Kun kun",
+    name: "The Kun",
+  },
+}
 
+// step 5.13
+test('renders content', () => {
   const component = render(
     <Blog blog={blog} />
   )
@@ -31,4 +32,18 @@ test('renders content', () => {
   // method 3 querySelector
   const div = component.container.querySelector(".blog");
   expect(div).toHaveTextContent("kun kun");
+})
+
+// step 5.14
+test('clicking the button to show likes and url', () => {
+  const component = render(
+    <Blog blog={blog} />
+  )
+  // component.debug()
+  const button = component.container.querySelector(".btn");
+  fireEvent.click(button)
+
+  const div = component.container.querySelector(".hide");
+  expect(div).toHaveTextContent(blog.likes);
+  expect(div).toHaveTextContent(blog.url);
 })
