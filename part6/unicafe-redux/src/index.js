485009bace023cdc52ffee0b047b0f597fff87d6
fanhuirong <fanhuirong321@gmail.com>
2022-01-19 22:34:34 +0800

    feat: step 6.1

--- part6/unicafe-redux/src/index.js
+++ part6/unicafe-redux/src/index.js
@@ -12,15 +12,21 @@ const App = () => {
     })
   }
 
+  const clickBtn = (type)=>{
+    store.dispatch({
+      type
+    })
+  }
+
   return (
     <div>
-      <button onClick={good}>good</button> 
-      <button>ok</button> 
-      <button>bad</button>
-      <button>reset stats</button>
+      <button onClick={()=>clickBtn('GOOD')}>good</button> 
+      <button onClick={()=>clickBtn('OK')}>ok</button> 
+      <button onClick={()=>clickBtn('BAD')}>bad</button>
+      <button onClick={()=>clickBtn('ZERO')}>reset stats</button>
       <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
     </div>
   )
 }
