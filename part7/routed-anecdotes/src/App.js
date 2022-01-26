3b3c58055aa55ff16ea773b539cacd9dadbf4800
fanhuirong <fanhuirong321@gmail.com>
2022-01-26 20:57:50 +0800

    feat: step 7.2

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -1,15 +1,18 @@
 import React, { useState } from 'react'
 import {
   BrowserRouter as Router,
-  Switch, Route, Link
+  Switch, Route, useRouteMatch
 } from "react-router-dom"
 import About from './components/About'
 import Menu from './components/Menu'
 import AnecdoteList from './components/AnecdoteList'
 import Footer from './components/Footer'
 import CreateNew from './components/CreateNew'
+import Anecdote from './components/Anecdote'
 
 const App = () => {
+
+
   const [anecdotes, setAnecdotes] = useState([
     {
       content: 'If it hurts, do it more often',
@@ -26,12 +29,19 @@ const App = () => {
       id: '2'
     }
   ])
+  const match = useRouteMatch('/anecdotes/:id')
+  const anecdote = match 
+    ? anecdotes.find(item => item.id === match.params.id)
+    : null
+    
+
   const addNew = (anecdote) => {
     anecdote.id = (Math.random() * 10000).toFixed(0)
     setAnecdotes(anecdotes.concat(anecdote))
   }
+
   return (
-      <Router>
+      <div>
         <Menu />
         <Switch>
           <Route path="/about"> 
@@ -40,13 +50,17 @@ const App = () => {
           <Route path="/create"> 
             <CreateNew addNew={addNew} />
           </Route>
+          <Route path="/anecdotes/:id">
+            <Anecdote anecdote={anecdote} />
+          </Route>
           {/* / last one */}
           <Route path="/"> 
             <AnecdoteList anecdotes={anecdotes} /> 
           </Route>
+
         </Switch>
         <Footer />
-      </Router>
+      </div>
   );
 }
 
