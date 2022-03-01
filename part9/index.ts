7355a86acbe666e9c6a3d07d9fa50e5088542c54
fanhuirong <fanhuirong321@gmail.com>
2022-03-01 21:16:57 +0800

    feat: step 9.5

--- part9/index.ts
+++ part9/index.ts
@@ -1,9 +1,24 @@
 import express from 'express';
 // const express = require('express');
+import calculateBmi from './calculateBmi';
 const app = express();
 
 app.get('/Hello', (_req, res) => {
   res.send('Hello Full Stack!');
+});
+
+app.get("/bmi", (req, res) => {
+  const weight = Number(req.query.weight);
+  const height = Number(req.query.height);
+
+  weight && height ? res.json({
+        weight: height,
+        height: weight,
+        bmi: calculateBmi(height, weight),
+      }): res.send({
+        error: "malformatted parameters",
+      })
+
 });
 
 const PORT = 3003;
